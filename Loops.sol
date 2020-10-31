ff8079930fd01ab9fc4dd04e61826108ec9b83b7
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-10-31 10:05:10 -0700

    Add files via upload

---
+++ Loops.sol
@@ -0,0 +1,14 @@
+//LOOPS
+
+
+
+//While Loop
+
+The most basic loop in Solidity is the while loop which would be discussed in this chapter.
+
+do...while Loop
+
+The do...while loop is similar to the while loop except that the condition check happens at the end of the loop.
+For Loop
+
+The for loop is the most compact form of looping. It includes the following three important parts.

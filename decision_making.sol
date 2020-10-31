9606db10fb4d3a1fd359cd433cf91edb2063f445
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-10-31 10:00:53 -0700

    Add files via upload

---
+++ decision_making.sol
@@ -0,0 +1,17 @@
+Solidity supports the following forms of if..else statement −
+
+Sr.No	Statements & Description
+1	
+if statement
+
+The if statement is the fundamental control statement that allows Solidity to make decisions and execute statements conditionally.
+
+2	
+if...else statement
+
+The 'if...else' statement is the next form of control statement that allows Solidity to execute statements in a more controlled way.
+
+3	
+if...else if... statement.
+
+The if...else if... statement is an advanced form of if...else that allows Solidity to make a correct decision out of several conditions.
